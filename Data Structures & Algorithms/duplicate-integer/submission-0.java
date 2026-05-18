d9cba80644fd836a40a9f3b4fa20a72fe6c44088
Kamogelo Kedige <kedigekamogelo32@gmail.com>
2026-05-18 23:23:02 +0200

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(i+1 < nums.length)
+            {
+                if(nums[i] == nums[i+1])
+                    return true;
+            }
+        }
+
+        return false;
+        
+    }
+}
